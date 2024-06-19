aaf989f3295d2e646f37d80b5cb4b2b1401e59e1
Dom Bunnett <bunnett@Doms-MacBook-Pro.local>
2024-06-19 11:07:16 +0100

    give links

--- app.py
+++ app.py
@@ -78,5 +78,5 @@ def search():
         return f"<p>Error: {str(e)}</p>", 400
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0',port=port)
